d13065b18dcbbaee0f1960c848d573c3f20bba54
DanielRY <danielrojasy@upeu.edu.pe>
2020-05-18 23:05:33 -0500

    version 3

---
+++ js/script.js
@@ -0,0 +1,77 @@
+document.getElementById("n1").addEventListener("click",n1);
+function n1(){ 
+    let sumado=document.getElementById("n1").innerHTML;
+    var elemento=document.getElementById("resultado");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+ }
+document.getElementById("n2").addEventListener("click",n2);
+function n2(){ 
+    let sumado=document.getElementById("n2").innerHTML;
+    var elemento=document.getElementById("resultado");
+    var value=elemento.value;
+    elemento.value=value + sumado;
+ }
+document.getElementById("n3").addEventListener("click",n3);
+function n3(){ 
+    let sumado=document.getElementById("n3").innerHTML;
+    var elemento=document.getElementById("resultado");
+    var value=elemento.value;
+    elemento.value =value+sumado;
+ }
+document.getElementById("n4").addEventListener("click",n4);
+function n4(){ 
+    let sumado = document.getElementById("n4").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
+document.getElementById("n5").addEventListener("click",n5);
+function n5(){ 
+    let sumado = document.getElementById("n5").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
+document.getElementById("n6").addEventListener("click",n6);
+function n6(){ 
+    let sumado = document.getElementById("n6").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
+document.getElementById("n7").addEventListener("click",n7);
+function n7(){ 
+    let sumado = document.getElementById("n7").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
+document.getElementById("n8").addEventListener("click",n8);
+function n8(){ 
+    let sumado = document.getElementById("n8").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
+document.getElementById("n9").addEventListener("click",n9);
+function n9(){ 
+    let sumado = document.getElementById("n9").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
+document.getElementById("n0").addEventListener("click",n0);
+function n0(){ 
+    let sumado = document.getElementById("n0").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
+ document.getElementById("np").addEventListener("click",np);
+function np(){ 
+    let sumado = document.getElementById("np").innerHTML;
+    var elemento = document.getElementById("resultado");
+    var value = elemento.value;
+    elemento.value = value + sumado;
+ }
